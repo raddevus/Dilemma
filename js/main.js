5a14095f256166dde01cd3df33c96c9ba436d5b6
raddevus <1radeutsch1@gmail.com>
2019-06-05 19:18:01 -0400

    Saves screen name to localStorage.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@
 
 function initializeForm(){
 	setDefaultButton();
-	$('#screenNameUnchosen').show();
-	$('#screenNameChosen').hide();
-	$("#screenNameText").focus();
+	var screenName = getScreenName();
+	if (screenName === null){
+		$('#screenNameUnchosen').show();
+		$('#screenNameChosen').hide();
+		$("#screenNameText").focus();
+		return;
+	}
+	displayScreenName(screenName);
+	
 }
 
 function setDefaultButton(){
@@ -16,6 +22,13 @@ function setDefaultButton(){
 });
 }
 
+function displayScreenName(screenName){
+	var msg = "Your Screen Name is saved as : " + screenName;
+	$('#screenNameUnchosen').alert('close');
+	$('#screenNameChosen').text(msg);
+	$('#screenNameChosen').show();
+}
+
 function setScreenName(){
 	if ($("#screenNameText").val() == ""){
 		alert("Please enter a valid Screen Name and try again.");
@@ -23,16 +36,45 @@ function setScreenName(){
 		return;
 	}
 	var screenName = $("#screenNameText").val();
-	//localStorage.setItem("screenName", screenName);
-	$('#screenNameUnchosen').alert('close');
-	var msg = "Your Screen Name is saved as : " + $("#screenNameText").val();
-	$('#screenNameChosen').text(msg);
-	$('#screenNameChosen').show();
+		
+	displayScreenName(screenName);
 	$('#screenNameText').val("");
 	$("#screenNameText").focus();
 	
+	writeScreeNameToStorage(screenName);
+	
+	// #### test code #######################
+	// alert(encodedVal);
+	// alert(getDecodedValue(encodedVal));
+	// ######################################
+		
+}
+
+function writeScreeNameToStorage(screenName){
+	var encodedScreenName = getEncodedValue(screenName);
+	localStorage.setItem("screenName", encodedScreenName);
 }
 
 function getScreenName(){
-	
+	// when localStorage items does not exist then screenName will be null
+	var screenName = localStorage.getItem("screenName");
+	if (screenName !== null){
+		screenName = getDecodedValue(screenName);
+	}
+	else{
+		
+	}
+	return screenName; // either a null or a valid value will be returned.
 }
+
+function getEncodedValue(clearText){
+	return btoa(encodeURI(clearText));
+}
+function getDecodedValue(encodedValue){
+		try {
+			return decodeURI(atob(encodedValue));
+		}
+		catch (e){
+			throw (e);
+		}
+	}
