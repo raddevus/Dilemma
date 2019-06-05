766a2bf6d3202d91c71d46c826a422af6cf631c1
raddevus <1radeutsch1@gmail.com>
2019-06-05 14:21:38 -0400

    Fixed so that user can type screen name and then press <ENTER>.  Also implements preventDefault on keypress or the page attempts to reload again.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 //main.js
 
 function initializeForm(){
+	setDefaultButton();
 	$('#screenNameUnchosen').show();
 	$('#screenNameChosen').hide();
 	$("#screenNameText").focus();
+}
+
+function setDefaultButton(){
+	$("#screenNameText").keypress(function(event){
+  if(event.keyCode == 13){
+		event.preventDefault();
+    $('#button-screenName').click();
+  }
+});
 }
 
 function setScreenName(){
 	if ($("#screenNameText").val() == ""){
 		alert("Please enter a valid Screen Name and try again.");
 		$("#screenNameText").focus();
+		return;
 	}
 	var screenName = $("#screenNameText").val();
-	//localStorage.setItem("screenName", $("#screenNameText").text());
+	//localStorage.setItem("screenName", screenName);
 	$('#screenNameUnchosen').alert('close');
 	var msg = "Your Screen Name is saved as : " + $("#screenNameText").val();
 	$('#screenNameChosen').text(msg);
 	$('#screenNameChosen').show();
 	$('#screenNameText').val("");
 	$("#screenNameText").focus();
+	
 }
 
 function getScreenName(){
